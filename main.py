167fea8e871fa9331d254275f4fb6944ecec9106
sovmirc <111938879+sovmirc@users.noreply.github.com>
2024-02-19 19:58:53 -0600

    Add files via upload
    prototype v1

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
+# will m
 import cv2
 import mediapipe as mp
 import pyautogui
 
-screen_w, screen_h = pyautogui.size()
 cam = cv2.VideoCapture(0)
+# locate facial features/landmarks
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
-
-def find_landmarks_and_click(landmarks, frame_w, frame_h):
-    for id, landmark in enumerate(landmarks[474:478]):
-        x = int(landmark.x * frame_w)
-        y = int(landmark.y * frame_h)
-        cv2.circle(frame, (x, y), 3, (0, 255, 0))
-        if id == 1:
-            screen_x = int(screen_w * landmark.x)
-            screen_y = int(screen_h * landmark.y)
-            pyautogui.moveTo(screen_x, screen_y)
-    left = [landmarks[145], landmarks[159]]
-    for landmark in left:
-        x = int(landmark.x * frame_w)
-        y = int(landmark.y * frame_h)
-        cv2.circle(frame, (x, y), 3, (0, 255, 255))
-    if (left[0].y - left[1].y) < 0.004:
-        pyautogui.click()
-        pyautogui.sleep(1)
+# detect screen size
+screen_w, screen_h = pyautogui.size()
 
 while True:
-    ret, frame = cam.read()
-    if not ret:
-        break
+    # create window, use webcam
+    _, frame = cam.read()
     frame = cv2.flip(frame, 1)
+
+    # convert video to different color
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = face_mesh.process(rgb_frame)
+
+    # detect if face is present or not, track location (x and y) in frame
     landmark_points = output.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
+
     if landmark_points:
         landmarks = landmark_points[0].landmark
-        frame_h, frame_w, _ = frame.shape
-        find_landmarks_and_click(landmarks, frame_w, frame_h)
-    cv2.imshow('Eye Controlled Mouse', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # detecting eye, ignore other facial features
+        for id, landmark in enumerate(landmarks[474:478]):
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
+            if id == 1:
+                screen_x = screen_w / frame_w * x
+                screen_y = screen_h / frame_h * y
+                pyautogui.moveTo(screen_x,screen_y)
 
-cam.release()
-cv2.destroyAllWindows()
+        # track top of eyelid, and bottom of eyelid to detect blink
+        left = [landmarks[145],landmarks[159]]
+        for landmark in left:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 255))
+
+        # measure distance between top & bottom eyelid to detect blinking. <0.004 is distance
+        # if true, click and wait 1.5s to prevent spam clicking
+        if (left[0].y - left[1].y) < 0.004:
+            pyautogui.click()
+            pyautogui.sleep(1.5)
+
+    # init frame & application name
+    cv2.imshow('EyeMouse', frame)
+    cv2.waitKey(1)
+
